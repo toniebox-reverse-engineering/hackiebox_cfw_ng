bac6cfe0258bde204c41587fbe9264e875f7da17
scilor <SciLor@users.noreply.github.com>
2021-02-14 23:02:48 +0100

    Prepare Branch Thumb Instr.

--- sd-bootloader-ng/bootmanager/armAsm.h
+++ sd-bootloader-ng/bootmanager/armAsm.h
@@ -10,6 +10,7 @@ extern "C"
 
 void ArmAsmT_bl(uint32_t pc, uint32_t target, uint32_t* instruction);
 void ArmAsmT_blx(uint32_t pc, uint32_t target, uint32_t* instruction);
+void ArmAsmT_b(uint32_t pc, uint32_t target, uint16_t* instruction);
 
 #ifdef __cplusplus
 }
